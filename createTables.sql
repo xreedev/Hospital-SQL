8f683b7c6b143bf266b1dd88a3ea0ffd6a31f6f6
Sreedev TS <xreedev@gmail.com>
2024-08-09 15:38:42 +0530

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -2,3 +2,8 @@ CREATE TABLE patients (
     patient_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+CREATE TABLE procedure (
+    procedure_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    duration INTEGER NOT NULL
+);
